46d28cd1e597bd7a65a3e6fb4dd26d1ad78ca3a2
skinzor <robertpopescu95@yahoo.com>
2017-07-25 16:17:22 +0300

    can: add NFC blobs from N
    Conflicts:
    	can/can-vendor.mk

--- can/can-vendor.mk
+++ can/can-vendor.mk
@@ -950,6 +950,15 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/can/proprietary/vendor/lib64/soundfx/libqcbassboost.so:system/vendor/lib64/soundfx/libqcbassboost.so \
     vendor/huawei/can/proprietary/vendor/lib64/soundfx/libqcreverb.so:system/vendor/lib64/soundfx/libqcreverb.so \
     vendor/huawei/can/proprietary/vendor/lib64/soundfx/libqcvirt.so:system/vendor/lib64/soundfx/libqcvirt.so \
+    vendor/huawei/can/proprietary/vendor/firmware/libpn548ad_fw.so:system/vendor/firmware/libpn548ad_fw.so \
+    vendor/huawei/can/proprietary/vendor/firmware/libpn551_fw.so:system/vendor/firmware/libpn551_fw.so \
+    vendor/huawei/can/proprietary/vendor/lib/mmi_nfc.so:system/vendor/lib/mmi_nfc.so \
+    vendor/huawei/can/proprietary/vendor/lib64/mmi_nfc.so:system/vendor/lib64/mmi_nfc.so \
+    vendor/huawei/can/proprietary/lib/libnfc-nci-45-551.so:system/lib/libnfc-nci-45-551.so \
+    vendor/huawei/can/proprietary/lib/libnfc_nci_jni_45_551.so:system/lib/libnfc_nci_jni_45_551.so \
+    vendor/huawei/can/proprietary/lib64/libnfc-nci-45-551.so:system/lib64/libnfc-nci-45-551.so \
+    vendor/huawei/can/proprietary/lib64/libnfc_nci_jni_45_551.so:system/lib64/libnfc_nci_jni_45_551.so \
+    vendor/huawei/can/proprietary/lib64/hw/nfc_nci.pn54x_45_551.default.so:system/lib64/hw/nfc_nci.pn54x_45_551.default.so \
 
 PRODUCT_PACKAGES += \
     libloc_api_v02 \
